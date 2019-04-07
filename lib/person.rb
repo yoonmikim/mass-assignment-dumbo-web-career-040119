b39e264cfbe0d4a89147442701614aed9eaf380d
clever-puppet-5071 <yoonmi.kim@rutgers.edu>
2019-04-07 08:02:26 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -2,6 +2,7 @@ class Person
   attr_accessor :name, :birthday, :hair_color, :eye_color, :height, :weight, :handed, :complexion, :t_shirt_size, :wrist_size, :glove_size, :pant_length, :pant_width
   
   def initialize()
-
+    self.send()
+  end
 
 end
