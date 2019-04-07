c0757b06908a4f1044cd1712461deb9c31c88d16
clever-puppet-5071 <yoonmi.kim@rutgers.edu>
2019-04-07 08:08:59 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -2,8 +2,8 @@ class Person
   
   attr_accessor :name, :birthday, :hair_color, :eye_color, :height, :weight, :handed, :complexion, :t_shirt_size, :wrist_size, :glove_size, :pant_length, :pant_width
   
-  def initialize(att)
-    att.each { |k, v| self.send((#{k}=), v) }
+  def initialize(attributes)
+    attributes.each { |k, v| self.send((#{k}=), v) }
   end
 
 end
